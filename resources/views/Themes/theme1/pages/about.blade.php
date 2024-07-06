163a47b43a0425d8f7904f4b240be9befcd2e90d
shahinchowdory53 <shahinchowdory53@gmail.com>
2024-07-06 17:15:29 +0600

    bvvbg

--- resources/views/Themes/theme1/pages/about.blade.php
+++ resources/views/Themes/theme1/pages/about.blade.php
@@ -8,7 +8,7 @@
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb justify-content-center mb-0">
                         <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
-                        <li class="breadcrumb-item active">About Us</li>
+                        <li class="breadcrumb-item active">About Usfbcvfg</li>
                     </ol>
                 </nav>
             </div>
