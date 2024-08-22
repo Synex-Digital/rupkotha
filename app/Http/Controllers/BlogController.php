f991e716d5674c972486cc9a4f0b914e7d4bf4c2
Esmail Khalifal <hossainmdismail9010@gmail.com>
2024-08-22 16:52:02 +0600

    htaccess

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -117,7 +117,7 @@ class BlogController extends Controller
             'seo_title'         => 'required',
             'seo_description'   => 'required',
             'seo_tags'          => 'required',
-            // 'slug'              => 'required|min:3|max:255|unique:blogs',
+            'slug'              => 'required',
         ]);
 
         $blog->category_id      = $request->category_id;
@@ -128,7 +128,7 @@ class BlogController extends Controller
         $blog->seo_description  = $request->seo_description;
         $blog->seo_tags         = $request->seo_tags;
         $blog->status           = $request->status;
-        $blog->slug             = Str::slug($request->title, '-');
+        $blog->slug             = $request->slug != null ? $request->slug : Str::slug($request->title, '-');
 
 
         if ($request->has('image')) {
